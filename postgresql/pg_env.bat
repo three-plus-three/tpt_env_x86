55b1522a64afde821291dfae2fa595243a5b758f
gziliang@aliyun.com <gziliang@aliyun.com>
2013-10-14 12:16:40 +0800

    init repo

---
+++ postgresql/pg_env.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+REM The script sets environment variables helpful for PostgreSQL
+
+@SET PATH="C:\Program Files\PostgreSQL\9.2\bin";%PATH%
+@SET PGDATA=F:\data\pg
+@SET PGDATABASE=postgres
+@SET PGUSER=postgres
+@SET PGPORT=5432
+@SET PGLOCALEDIR=C:\Program Files\PostgreSQL\9.2\share\locale
+
+                          
