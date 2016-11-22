21d353dfdee86d164332e851501978673ca6562b
eeiceyes <eeiceyes@163.com>
2016-11-22 18:08:29 +0800

    update phantomjs

--- phantomjs/gen_pdf.js
+++ phantomjs/gen_pdf.js
@@ -6,7 +6,7 @@ console.log(system.args)
 
 page.viewportSize = { width: 1280, height: 1080 };
 page.open(system.args[1] + ".html", function start(status) {
-  page.paperSize = { format: 'A4', orientation: 'portrait', margin: '0.8cm' }
+  page.paperSize = { format: 'A4', orientation: 'portrait', margin: '0.1cm' }
   page.settings.loadImages = true;
   //page.settings.userAgent = 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36';
   page.render(system.args[1].replace("file:///", "") + ".pdf", {format: 'pdf', quality: '100'});
